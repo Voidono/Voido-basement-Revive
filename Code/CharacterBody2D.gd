ae6e46169353b51c392738eb9d4f2358337c61ed
manh1122005 <voidoistaken@gmail.com>
2024-10-26 12:54:51 +0700

    init

--- Code/CharacterBody2D.gd
+++ Code/CharacterBody2D.gd
@@ -1,28 +1,44 @@
 extends CharacterBody2D
 
-@onready var anim = get_node("Flan/AnimatedSprite2D")
+
+const Jump_Velocity = -1000.0
+@export var speed = 300.0
+@export var jump_height = -400.0
+@onready var anim = get_node("Combat_player/AnimatedSprite2D")
 var dir = 0 # Store the last direction outside the function
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
 
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 300
+	#Add gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	#Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_height
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
 	
 	# Update the animation when the character is moving
 	if velocity.length() > 0:
 		if Input.is_action_pressed("move_right"):
-			anim.play("Walk_Right")
+			anim.play("Move_right")
 			dir = 0 # Update the last direction to Right
 		elif Input.is_action_pressed("move_left"):
-			anim.play("Walk_Left")
+			anim.play("Move_left")
 			dir = 1 # Update the last direction to Left
 		elif Input.is_action_pressed("move_up"):
-			anim.play("Walk_Up")
+			anim.play("Move_up")
 			dir = 2 # Update the last direction to Up
 		elif Input.is_action_pressed("move_down"):
-			anim.play("Walk_Down")
+			anim.play("Move_down")
 			dir = 3 # Update the last direction to Down
-
-	# If the character is not moving, play the idle animation based on the last direction
+			
 	if velocity.length() == 0:
 		match dir:
 			0:
@@ -35,7 +51,3 @@ func _physics_process(delta):
 				anim.play("Idle_Down")
 		
 	move_and_slide()
-
-
-
-
